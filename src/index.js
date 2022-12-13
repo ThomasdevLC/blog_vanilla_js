893cc9b65dc6b173b6c280136fa0967cc50cdd0f
ThomasdevLC <thomas.lecam@gmail.com>
2022-12-13 14:55:21 +0100

    menu responsive

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-import { async } from "regenerator-runtime";
 import "./assets/styles/styles.scss";
 import "./index.scss";
 
 const articleContainerElement = document.querySelector(".articles-container");
 
-const createArticle = (articles) => {
-  const articlesDom = articles.map((article) => {
-    const articleDom = document.createElement("div");
-    articleDom.classList.add("article");
-    articleDom.innerHTML = `
-        <img
-        src="${article.img}"
-        alt="profile"
-      />
-      <h2>${article.title}</h2>
-      <p class="article-author">${article.author} - ${article.category}</p>
-      <p class="article-content">
-      ${article.content}
-      </p>
-      <div class="article-actions">
-              <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
-            </div>  
-      
-      `;
-    return articleDom;
-  });
+const createArticles = (articles) => {
+  const createArticleDOM = (article) => {
+    const articleDOM = document.createElement("div");
+    articleDOM.classList.add("article");
+    articleDOM.innerHTML = `
+	<img
+		src="${article.img}"
+		alt="profile"
+	/>
+	<h2>${article.title}</h2>
+	<p class="article-author">${article.author} - ${article.category}</p>
+	<p class="article-content">
+		${article.content}
+	</p>
+	<div class="article-actions">
+		<button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
+	</div>
+`;
+    return articleDOM;
+  };
+  const articlesDOM = Array.isArray(articles)
+    ? articles.map((article) => createArticleDOM(article))
+    : articles
+    ? [createArticleDOM(articles)]
+    : [];
 
   articleContainerElement.innerHTML = "";
-  articleContainerElement.append(...articlesDom);
+  articleContainerElement.append(...articlesDOM);
   const deleteButtons = articleContainerElement.querySelectorAll(".btn-danger");
   deleteButtons.forEach((button) => {
     button.addEventListener("click", async (event) => {
@@ -36,13 +39,15 @@ const createArticle = (articles) => {
         const articleId = target.dataset.id;
         const response = await fetch(
           `https://restapi.fr/api/article/${articleId}`,
-          { method: "DELETE" }
+          {
+            method: "DELETE",
+          }
         );
         const body = await response.json();
-        fetchArticle();
         console.log(body);
-      } catch (err) {
-        console.log("error :", err);
+        fetchArticle();
+      } catch (e) {
+        console.log("e : ", e);
       }
     });
   });
@@ -52,10 +57,10 @@ const fetchArticle = async () => {
   try {
     const response = await fetch("https://restapi.fr/api/article");
     const articles = await response.json();
-    console.log(articles);
-    createArticle(articles);
-  } catch (err) {
-    console.log("error :", err);
+    createArticles(articles);
+    console.log("response : ", articles);
+  } catch (e) {
+    console.log("e : ", e);
   }
 };
 
